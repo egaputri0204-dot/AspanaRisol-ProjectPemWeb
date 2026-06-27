608c3e769a6a9e1c18be9826676716c3090fccfd
Ega Putri Juliyanti <egaputri0204@student.esaunggul.ac.id>
2026-06-27 21:23:11 +0700

    Fix Vercel entrypoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,3 @@
-const serverless = require("serverless-http");
 const app = require("../app");
 
-module.exports = serverless(app, {
-  binary: ["image/*", "application/octet-stream"],
-  provider: "vercel",
-});
+module.exports = app;
